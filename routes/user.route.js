3c155e7f6a1c6d931a8b57ea602362e6084e8d96
Glitch (hello-express) <none>
2020-05-16 04:14:59 +0000

    🚇🍶 Checkpoint ./views/users/edit.pug:16205955/449 ./views/users/create.pug:16205955/68 ./routes/user.route.js:16205955/1183

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,6 +7,45 @@ const db = require('./db');
 
 app.get('/', (req,res) => {
   res.reder('users/index', {
-    users : db.get('users').value();
+    users : db.get('users').value()
   });
+});
+
+app.get('/create', (req,res) => {
+  res.render('users/create');
+});
+
+app.post('/create', (req,res) => {
+  var id = shortid.generate();
+  db.get('users').push({
+    id: id,
+    name: req.body.name,
+    age: req.body.age
+  }).write();
+  res.render('users/create');
+});
+
+app.get('/:id', (req,res) => {
+  var id = req.params.id;
+  var user = db.get('users').find({id:id}).value();
+  res.render('users/show',{
+    user: user
+  });
+});
+
+app.get('/:id/edit', (req,res) => {
+  var id = req.params.id;
+  var user = db.get('users').find({id:id}).value();
+  res.render('users/edit',{
+    user: user
+  });
+});
+
+app.post('/update/:id', (req,res) => {
+  var id = req.params.id;
+  var user = db.get('users').find({id:id}).assign({
+    name: req.body.name,
+    age: req.body.age
+  }).write();
+  res.redirect('users'});
 });
