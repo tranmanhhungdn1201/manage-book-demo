a0c0613217ec1cb874670a7f887dafc4f1d5a5e0
Glitch (hello-express) <none>
2020-05-18 16:36:28 +0000

    🌿🏇 Checkpoint ./controllers/cart.controller.js:16205955/870 ./middleware/session.middleware.js:16205955/10

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,20 +1,33 @@
 const db = require('../db');
 
 module.exports.index = (req,res) => {
-  res.render('cart/index');
-};
-
-module.exports.addToCart = (req,res) => {
-  var bookId = req.params.bookId;
-  var sessionId = req.singedCookies.sessionId;
-  
+  var sessionId = req.signedCookies.sessionId;
   if(!sessionId){
      res.redirect('/books');
       return;
   }
+  var carts = db.get('sessions')
+    .find({id: sessionId})
+    .value();
+
+  console.log(db.get('sessions').value());
+  return;
+    res.render('cart/index');
+};
+
+module.exports.addToCart = (req,res) => {
+  var bookId = req.params.bookId;
+  var sessionId = req.signedCookies.sessionId;
+  if(!sessionId){
+     res.redirect('/books');
+      return;
+  }
+  var count = db.get('sessions')
+    .find({id: sessionId}).value();
+  
   db.get('sessions')
-    .find({sessionId: sessionId})
-    .set('cart.' + bookId, 1)
+    .find({id: sessionId})
+    .set('cart.' + bookId, count + 1)
     .write();
   res.redirect('/books');
 };
