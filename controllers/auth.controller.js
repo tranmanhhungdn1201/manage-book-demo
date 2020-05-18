239b4c113044812af82d35faa45d926dbdd8bd8c
Glitch (hello-express) <none>
2020-05-18 15:04:48 +0000

    👟🐈 Checkpoint ./controllers/auth.controller.js:16205955/829 ./db.json:16205955/5178 ./views/profile/index.pug:16205955/26

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -13,16 +13,19 @@ module.exports.profile = (req,res) => {
 
 module.exports.updateAvatar = (req,res) => {
   const path = req.file.path;
-  var user = db.get('users').find({id: req.signedCookies.userId}).value();
+  var user = db.get('users').find({id: req.signedCookies.userId});
   const uniqueFilename = new Date().toISOString();
     cloudinary.uploader.upload(
       path,
-      { public_id: `profile/${uniqueFilename}`, tags: `users` },
+      { public_id: 'profile/'+user.value().id, tags: `users` },
       function(err, image) {
         if (err) return res.send(err)
         console.log('file uploaded to Cloudinary');
-        user.
+        console.log(image);
+        user.assign({
+          avatar: image.url
+        }).write();
       }
     )
-
+    res.render('profile/index');
 };
