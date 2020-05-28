ec480e2424c3a6c37c9044eb8df5cdf3c1a61aeb
Glitch (hello-express) <none>
2020-05-28 01:08:36 +0000

    📉⛷ Checkpoint ./server.js:16205955/129 ./api/routes/login.route.js:16205955/234 ./api/controllers/login.controller.js:16205955/1753

---
+++ api/controllers/login.controller.js
@@ -0,0 +1,65 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const db = require('../../db');
+
+module.exports.index = (req,res) => {
+  res.render('login/index');
+};
+
+module.exports.postLogin = (req, res, next) => {
+  var errors = [];
+  var wrongLoginCount = req.cookies.wrongLoginCount ? req.cookies.wrongLoginCount : 0;
+  if(+wrongLoginCount === 4){
+    errors.push('Login fail too many times!');
+    res.render('login/index', {
+      errors : errors,
+      value: res.body
+    });
+    return;
+  }
+  var checkLogin = true;
+  if(!req.body.email){
+    errors.push('Email is required');
+    checkLogin = false;
+  }
+  if(!req.body.password){
+      errors.push('Password is required');
+      checkLogin = false;
+  }
+  if(!checkLogin){
+    res.render('login/index', {
+      errors : errors,
+      value: req.body
+    });
+    return;
+  }
+  var email = req.body.email;
+  var password = req.body.password;
+  var user = db.get('users').find({email: email}).value();
+  if(!user){
+    errors.push('User\'s not exist')
+    res.render('login/index', {
+      errors : errors,
+      value: res.body
+    });
+    return;
+  }
+  bcrypt.compare(password, user.password, function(err, result) {
+      if(!result){
+        res.cookie('wrongLoginCount', ++wrongLoginCount);
+        console.log(wrongLoginCount);
+        errors.push('Wrong password')
+        console.log(errors);
+        res.render('login/index', {
+          errors : errors,
+          value: req.body
+        });
+        return true;
+      }
+    res.cookie('userId', user.id, {
+      signed : true
+    });
+    res.json(user);
+  });
+
+};
